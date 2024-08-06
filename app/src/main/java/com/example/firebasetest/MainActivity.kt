ca35cfe3037c1d48f30370029f259799c2528081
LoveAviation <ivangeorgmarcov@gmail.com>
2024-08-06 20:17:42 +0300

    ready

---
+++ app/src/main/java/com/example/firebasetest/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.firebasetest
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.firebasetest.databinding.ActivityMainBinding
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var count : String
+    private val launcher = registerForActivityResult(ActivityResultContracts.RequestPermission()){}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            createNotification()
+            binding.textView.text = count
+        }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            Log.d("TOKEN", it.result)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onStart() {
+        super.onStart()
+        checkPermission()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun createNotification(){
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        val pendingIntent = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        else
+            PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+
+        val notification = NotificationCompat.Builder(this, App.NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.baseline_notifications_24)
+            .setContentTitle(getString(R.string.important))
+            .setContentText(getString(R.string.text_error))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+        NotificationManagerCompat.from(this).notify(NOTIFICATION_ID, notification)
+    }
+
+    companion object{
+        private const val NOTIFICATION_ID = 1000
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun checkPermission(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+            launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+
+
+}
+
